de382d124878d98bc0be82492effbdd2079cf8fa
croseadu <croseadu@93ef7e32-9b48-0410-81a5-0f72e0cf0767>
2010-07-22 11:29:54 +0000

    add code for list

--- trunk/codebase/util/util.c
+++ trunk/codebase/util/util.c
@@ -1,1 +1,63 @@
 
+
+
+
+
+STATUS getNextNumber(const char *inputBuffer, int *pCurIndex, int *pData)
+{
+	int index, data = 0;
+	BOOL bGetData = FALSE;
+
+	index = *pCurIndex;
+
+	while (1)
+	{
+		switch (inputBuffer[index])
+		{
+			case ' ':
+			case '\t':
+				if (bGetData)
+				{
+					*pCurIndex = index;
+					*pData = data;
+					return OK;
+				}
+				index++;
+				break;
+			case '\n':
+			case '\0';
+				if (bGetData)
+					return OK;
+				else
+					return END;
+			case '0':
+				data = 0;
+				bGetData = TRUE;
+				index++;
+				break;
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				if (bGetData)
+					return ERROR;
+				do
+				{
+					data *= 10;
+					data += inputBuffer[index++] - '0';	
+				}while(isdigit(inputBuffer[index));
+				bGetData = TRUE;
+				break;
+			default:
+				return ERROR;
+			
+		}
+	}
+
+
+}
