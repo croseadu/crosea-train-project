f242bfb74294be59cbaf6477960842b12afcff77
croseadu <croseadu@93ef7e32-9b48-0410-81a5-0f72e0cf0767>
2010-08-13 10:58:10 +0000

    add part of compute expr

--- trunk/codebase/basic/computeExpr/computeExpr.c
+++ trunk/codebase/basic/computeExpr/computeExpr.c
@@ -2,12 +2,40 @@
 #include "../../include/util.h"
 #include "../../include/stack.h"
 
+#define LESS -1
+#define GREAT 1
+#define EQUAL 0
+#define UNKNOWN 2
 
+//  + - * / ( ) #
+// +
+// -
+// *
+// /
+// (
+// )
+// #
+int priorityTable[7][7]
+{
+{1,1,-1,-1,-1,1,1},
+{1,1,-1,-1,-1,1,1},
+{1,1,1,1,-1,1,1},	
+{1,1,1,1,-1,1,1},
+{-1,-1,-1,-1,-1,0,2},
+{2,2,2,2,2,2,2},
+{-1,-1,-1,-1,-1-}
+}
+
+int compute(const char operator, int op0, int op1);
 
 int main()
 {
 	char c, *inputBuffer;
-	int maxElements, curIndex;
+	char curOperator;
+	TOKEN curToken;
+	int maxElements, curIndex, data;
+	LP_STACK pOperatorStack, pOperandStack;
+	STATUS result;
 
 	inputBuffer = (char *)malloc(sizeof(char) * INIT_BUFFER_SIZE);
 	if (NULL == inputBuffer)
@@ -24,7 +52,7 @@ int main()
 		inputBuffer[curIndex++] = c;
 		c = getchar();
 
-		if (curIndex >= maxElements - 1)
+		if (curIndex >= maxElements - 2)
 		{
 			maxElements += INCRE_BUFFER_SIZE;
 			inputBuffer = (char *)realloc(inputBuffer, sizeof(char) * maxElements);
@@ -35,13 +63,119 @@ int main()
 			}
 		}
 	}
-	inputBuffer[curIndex] = '#';
+	inputBuffer[curIndex++] = '#';
+	inputBuffer[curIndex] = '\0';
 
-	while(!isStackEmpty(pOperator))
+	result = createStack(&pOperatorStack, sizeof(char));
+	if (result != ok)
 	{
-		
+		Print(("Out of Memory when create Stack\n"));
+		free(inputBuffer);
+		exit(-1);	
 	}
 
+	result = createStack(&pOperandStack, sizeof(int));
+	if (result != ok)
+	{
+		Print(("Out of Memory when create Stack\n"));
+		free(inputBuffer);
+		destoryStack(pOperatorStack);
+		exit(-1);	
+	}
+	
+	curOperator = '#';
+	push(pOperatorStack, &curOperator);
+	
+	curIndex = 0;
+	getNextOperOrOpnd(inputBuffer, &curIndex, &curToken);
+	while(!isStackEmpty(pOperator))
+	{
+		if (curToken.tokenType == OPEREND)
+		{
+			push(pOperandStack, &curToken.operand);
+		}
+		else
+		{
+			getTop(pOperatorStack, &curOperator);
+			switch(priorityCompare(curOperator, curToken.operator))
+			{
+			case LESS:
+				push(pOperatorStack, &curToken.operator);
+				getNextOperOrOpnd(inputBuffer, &curIndex, &curToken);
+				break;
+			case GREAT:
+				{
+					int data1, data2;
+					
+					if (isStackEmpty(pOperandStack))
+					{
+						Print(("Error when compute expr\n"));
+						exit(-1);
+					}
+					pop(pOperandStack, &data1);
+					if (isStackEmpty(pOperandStack))
+					{
+						Print(("Error when compute expr\n"));
+						exit(-1);
+					}
+					pop(pOperandStack, &data2);
+					
+					data = compute(curOperator, data1, data2);
+					push(pOperandStack, &data);
+					pop(pOperatorStack, &curOperator);
+					break;
+				}
+			case EQUAL:
+				pop(pOperatorStack, &curOperator);
+				getNextOperOrOpnd(inputBuffer, &curIndex, &curToken);
+				break;
+			default:
+				{
+					Print(("Error when compute expr\n"));
+					exit(-1);	
+				}
+				
+				
+			}
+		}
+	}
+
+	getTop(pOperand, &data);
+	if (isStackEmpty(pOperandStack))
+	{
+		Print(("Error when compute expr\n"));
+		exit(-1);
+	}
+
+	Print(("\nFinal Result = %d\n", data));
+	
+	free(inputBuffer)
+	destoryStack(pOperatorStack);
+	destoryStack(pOperandStack);
+
+	return 0;
 
 }
+
+int compute(const char operator, int op0, int op1)
+{
+	switch(operator)
+	{
+	case '+':
+		return op0+op1;
+	case '-':
+		return op0-op1;
+	case '*':
+		return op0*op1;
+	case '/':
+		return op0/op1;
+	default:
+		{
+			Print(("Error when compute two operand\n"));
+			exit(-1);
+		}
+	}
+}
+
+
 
