1101229e88ea285bf22522f78a9dd067cc12480c
CroseaDu <CroseaDu@93ef7e32-9b48-0410-81a5-0f72e0cf0767>
2010-08-17 14:10:35 +0000

    add code which calculate n!, not done

--- trunk/codebase/basic/calcFactorial/polyList.h
+++ trunk/codebase/basic/calcFactorial/polyList.h
@@ -1,14 +1,17 @@
-typedef struct _SingleList
+typedef struct _DoubleList
 {
 // 1+2x+3x*x+6x*x*x ----(1, 0), (2, 1), (3,2), (6,3)
 	int factor;
 	unsigned int nIndex;
-	struct _SingleList *pNextNode;
-}SINGLE_LIST, *LP_SINGLE_LIST;
+	struct _DoubleList *pNextNode;
+}DOUBLE_LIST, *LP_DOUBLE_LIST;
 
 
-STATUS initList(LP_SINGLE_LIST *);
-void destoryList(LP_SINGLE_LIST);
-STATUS insertToPolyList(LP_SINGLE_LIST, int, int);
-void printPolyList(const LP_SINGLE_LIST);
+STATUS initList(LP_DOUBLE_LIST *);
+void destoryList(LP_DOUBLE_LIST);
+void cleanList(LP_DOUBLE_LIST );
+STATUS insertToPolyList(LP_DOUBLE_LIST, int, int);
+void printPolyList(const LP_DOUBLE_LIST);
+STATUS setMulList(LP_DOUBLE_LIST , unsigned int k);
+STATUS mulSelfList(LP_DOUBLE_LIST ,LP_DOUBLE_LIST );
 
