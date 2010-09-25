89c0bf7b3b8e15a88dfd0258b280d043eeeef9f0
croseadu <croseadu@93ef7e32-9b48-0410-81a5-0f72e0cf0767>
2010-09-25 11:08:00 +0000

    work on binaryTree

---
+++ codebase/include/tree.h
@@ -0,0 +1,14 @@
+typedef struct _TREE_NODE
+{
+	char *pData;
+	struct _TREE_NODE *pLeftChild;
+	struct _TREE_NODE *pRightChild;
+}TREE_NODE, *LP_TREE_NODE;
+
+void preOrderTraverse(LP_TREE_NODE pTreeNode, void (*pFn)(LP_TREE_NODE));
+void postOrderTraverse(LP_TREE_NODE pTreeNode, void (*pFn)(LP_TREE_NODE));
+void inOrderTraverse(LP_TREE_NODE pTreeNode, void (*pFn)(LP_TREE_NODE));
+
+void preOrderUseStack(LP_TREE_NODE pTreeNode, void (*pFn)(LP_TREE_NODE), LP_STACK pStack);
+void inOrderUseStack(LP_TREE_NODE pTreeNode, void (*pFn)(LP_TREE_NODE), LP_STACK pStack);
+void hierarchyTraverse(LP_TREE_NODE pRootNode,void (*pFn)(LP_TREE_NODE), LP_QUEUE pQueue);
