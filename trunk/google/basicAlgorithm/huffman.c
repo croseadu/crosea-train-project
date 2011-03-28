04c2db6af1dc361598421f39fed147d642cf771a
croseadu <croseadu@93ef7e32-9b48-0410-81a5-0f72e0cf0767>
2011-03-28 04:52:54 +0000

    Start Huffman Tree

--- trunk/google/basicAlgorithm/huffman.c
+++ trunk/google/basicAlgorithm/huffman.c
@@ -1,1 +1,172 @@
+#include "../include/common.h"
 
+// %10 [0,60)  %30 [60,70) 30%[70,80) %15 [80,90) %5 [90,100]
+// fail         meet       good       exceed       outstanding
+
+// 1. Build Huffman Tree for search
+// 2. Build Huffman Tree for code
+
+typedef struct _TREE_NODE
+{
+  char *grade;
+  int weight;
+  int parent;
+  int leftChild;
+  int rightChild;
+}TREE_NODE, *LP_TREE_NODE;
+
+typedef struct 
+{
+  int first;
+  int second;
+}RESULT, *LP_RESULT;
+
+// Huffman TREE always have 2n-1 node.
+
+void inOrderTraverse(LP_TREE_NODE, int);
+RESULT selectTwoMinimal(LP_TREE_NODE pStartNode, int from, int end);
+
+int main()
+{
+  LP_TREE_NODE pStartNode;
+  const int NODE_NUM = 5;
+  int i = 0;
+  RESULT result;
+  char middle[10][30];
+  static int middleIndex = 0;
+  
+  pStartNode = (LP_TREE_NODE)malloc((2*NODE_NUM - 1)*sizeof(TREE_NODE));
+  if (NULL == pStartNode)
+    {
+      printf("Out Of Memory in Line %d, File %s", __LINE__, __FILE__);
+      exit(OVERFLOW);
+    }
+
+  
+  memset(middle, 0 ,sizeof(char)*10*30);
+  memset(pStartNode , 0, sizeof(TREE_NODE)*(2*NODE_NUM - 1));
+
+  (pStartNode)->grade = "fail";
+  (pStartNode)->weight = 10;
+  (pStartNode)->leftChild = -1;
+  (pStartNode)->rightChild = -1;
+  (pStartNode)->parent = -1;
+  
+  (pStartNode+1)->grade = "meet";
+  (pStartNode+1)->weight = 30;
+  (pStartNode+1)->leftChild = -1;
+  (pStartNode+1)->rightChild = -1;
+  (pStartNode+1)->parent = -1;
+
+  (pStartNode+2)->grade = "good";
+  (pStartNode+2)->weight = 30;
+  (pStartNode+2)->leftChild = -1;
+  (pStartNode+2)->rightChild = -1;
+  (pStartNode+2)->parent = -1;
+  
+  (pStartNode+3)->grade = "exceed";
+  (pStartNode+3)->weight = 15;
+  (pStartNode+3)->leftChild = -1;
+  (pStartNode+3)->rightChild = -1;
+  (pStartNode+3)->parent = -1;
+
+  (pStartNode+4)->grade = "outstanding";
+  (pStartNode+4)->weight = 5;
+  (pStartNode+4)->leftChild = -1;
+  (pStartNode+4)->rightChild = -1;
+  (pStartNode+4)->parent = -1;
+
+  
+  for (i = 0; i < NODE_NUM - 1; i++)
+    {
+      int dataIndex = 0;
+
+      result = selectTwoMinimal(pStartNode, 0, NODE_NUM-1+i);
+      (pStartNode + NODE_NUM + i)->leftChild = result.first;
+      (pStartNode + NODE_NUM + i)->rightChild = result.second;
+      (pStartNode + NODE_NUM + i)->parent = -1;
+      (pStartNode + NODE_NUM + i)->weight = 
+	(pStartNode + result.first)->weight + (pStartNode + result.second)->weight;
+
+      (pStartNode + NODE_NUM + i)->grade = middle[middleIndex++];
+      if ((pStartNode+result.first)->grade[1] != '+')
+	{
+	  (pStartNode + NODE_NUM + i)->grade[0] = (pStartNode+result.first)->grade[0];
+	  dataIndex++;
+	}
+      else
+	{
+	  strcat((pStartNode + NODE_NUM + i)->grade, (pStartNode+result.first)->grade);
+	  dataIndex += strlen((pStartNode+result.first)->grade);
+	}
+      (pStartNode + NODE_NUM + i)->grade[dataIndex++] = '+';
+      if ((pStartNode+result.second)->grade[1] != '+')
+	{
+	  (pStartNode + NODE_NUM + i)->grade[dataIndex] = (pStartNode+result.second)->grade[0];
+	}
+      else
+	{
+	  strcat((pStartNode + NODE_NUM + i)->grade+dataIndex, (pStartNode+result.second)->grade);
+	}
+
+
+      (pStartNode + result.first)->parent = NODE_NUM + i;
+      (pStartNode + result.second)->parent = NODE_NUM + i;  
+      printf("\nSelect %d %s %d %s,Parent %d", 
+	     result.first, 
+	     (pStartNode+result.first)->grade?(pStartNode+result.first)->grade:"",
+	     result.second,
+	     (pStartNode+result.second)->grade?(pStartNode+result.second)->grade:"",
+	     i);
+    }
+
+  inOrderTraverse(pStartNode,  2 * NODE_NUM - 2);
+  putchar('\n');
+  // Second Part
+  
+
+
+
+
+
+
+  return 0;
+}
+void inOrderTraverse(LP_TREE_NODE pStartNode, int rootIndex)
+{
+  static int count = 0;
+  if((pStartNode + rootIndex)->leftChild >= 0)
+    inOrderTraverse(pStartNode,(pStartNode + rootIndex)->leftChild);
+    printf("\n%s", (pStartNode + rootIndex)->grade);
+
+  if((pStartNode + rootIndex)->rightChild >= 0)
+    inOrderTraverse(pStartNode,(pStartNode + rootIndex)->rightChild);
+}
+
+RESULT selectTwoMinimal(LP_TREE_NODE pStartNode, int from, int end)
+{
+  int i;
+  RESULT result;
+  
+  result.first = -1;
+  result.second = -1;
+  
+  for (i = from; i <= end; i++)
+    {
+      if ((pStartNode+i)->parent >= 0)
+	continue;
+      if (result.first == -1)
+	result.first = i;
+      else if((pStartNode + result.first)->weight > (pStartNode+i)->weight)
+	{
+	  result.second = result.first;
+	  result.first = i;
+	}
+      else if (result.second == -1 ||
+	       (pStartNode + result.second)->weight > (pStartNode+i)->weight )
+	{
+	  result.second = i;
+	}
+    }
+  return result;
+}
--- trunk/google/basicAlgorithm/threadingBiTree.c
+++ trunk/google/basicAlgorithm/threadingBiTree.c
@@ -1,1 +1,243 @@
+#include "../include/common.h"
+#include "../include/stack.h"
 
+
+typedef struct _T_BI_TREE
+{
+  char *pData;
+  struct _T_BI_TREE *pLeftChild;
+  struct _T_BI_TREE *pRightChild;
+  BOOL bLeftTag;
+  BOOL bRightTag;
+}T_BI_TREE_NODE, *LP_T_BI_TREE_NODE;
+
+static STATUS createTreeRecursive(LP_T_BI_TREE_NODE *, char *, unsigned int *);
+void preOrderTraverse(LP_T_BI_TREE_NODE pRoot, VISIT_FUNC visit);
+void inOrderTraverse(LP_T_BI_TREE_NODE pRoot, VISIT_FUNC visit);
+void destroyTree(LP_T_BI_TREE_NODE pRoot);
+
+void threadingTree(LP_T_BI_TREE_NODE pRoot, LP_T_BI_TREE_NODE *ppThreadRoot);
+void threadVisit(LP_T_BI_TREE_NODE pThreadRoot, VISIT_FUNC visit);
+
+static int count = 0;
+void visitNode(void *pData)
+{
+  putchar((*(char *)pData));
+}
+
+void limitVisitNode(void *pData)
+{
+  if (count++ < 20)
+    {
+      putchar((*(char *)pData));
+    }
+}
+
+int main()
+{
+  LP_T_BI_TREE_NODE pRoot = NULL, pThreadRoot = NULL;
+
+  if (createTree(&pRoot) != OK)
+    {
+      printf("Exit: Failed to create tree\n");
+      exit(-1);
+    }
+  printf("\nStart PreOrderTraverse:");
+  preOrderTraverse(pRoot, visitNode);
+  printf("\nStart InOderTraverse:");
+  inOrderTraverse(pRoot, visitNode);
+  
+  printf("\nThreading The Binary Tree");
+  threadingTree(pRoot, &pThreadRoot);
+  printf("\nInOrder ThreadingVisit:");
+  threadVisit(pThreadRoot, limitVisitNode);
+  printf("\n");
+  destroyTree(pRoot);
+  return 0;
+}
+
+/* Input is a preOrder data, and use ' ' to mark a NULL child */
+STATUS createTree(LP_T_BI_TREE_NODE *ppRoot)
+{
+  LP_T_BI_TREE_NODE pRoot = NULL;
+  char *pInputData, c;
+  unsigned int index = 0, maxSize;
+  FILE *fp;
+  
+  if ((fp = fopen("inputDataTree.txt", "r")) == NULL)
+    {
+      printf("Cannot Open File : inputData.txt\n");
+      return ERROR;
+    }
+
+#define INIT_SIZE 10
+#define INCRE_SIZE 5
+  
+  pInputData = (char *)malloc(sizeof(char)*INCRE_SIZE);
+  if (NULL == pInputData)
+    {
+      printf("Out Of Memory in Line %d, Function %s", __LINE__, __FUNCTION__);
+      return OVERFLOW;
+    }
+  maxSize = INIT_SIZE;
+
+  while ((c = fgetc(fp)) != EOF)
+    {
+      pInputData[index++] = c;
+      if (index >= maxSize)
+	{
+	  pInputData = (char *)realloc(pInputData, (maxSize + INCRE_SIZE)*sizeof(char));
+	  if (NULL == pInputData)
+	    {
+	      printf("Out Of Memory in Line %d, Function %s", __LINE__, __FUNCTION__);
+	      return OVERFLOW;
+	    }	  
+	  maxSize += INCRE_SIZE;
+	}
+    }
+
+  pInputData[index] = ' ';
+
+  index = 0;
+  if (createTreeRecursive(&pRoot, pInputData ,&index) != OK)
+    {
+      free(pInputData);
+      printf("Create Tree Recursive Fail\n");
+      return ERROR;
+    }
+
+  *ppRoot = pRoot;
+  fclose(fp);
+  return OK;
+}
+
+static STATUS createTreeRecursive(LP_T_BI_TREE_NODE *ppNode, char *pData, unsigned int *pCurIndex)
+{
+  LP_T_BI_TREE_NODE pNode;
+  STATUS status;
+
+  if (pData[*pCurIndex] == ' ')
+    {
+      (*pCurIndex)++;
+      *ppNode = NULL;
+      return OK;
+    }
+
+  pNode = (LP_T_BI_TREE_NODE)malloc(sizeof(T_BI_TREE_NODE));
+  if (NULL == pNode)
+    {
+      printf("Out Of Memory in line %d function %s", __LINE__, __FUNCTION__);
+      return OVERFLOW;
+    }
+  pNode->pData = (char *)malloc(sizeof(char));
+  if (NULL == pNode->pData)
+    {
+      printf("Out Of Memory in line %d function %s", __LINE__, __FUNCTION__);
+      free(pNode);
+      return OVERFLOW;
+    }
+  *pNode->pData = pData[*pCurIndex];
+  (*pCurIndex)++;
+
+  pNode->bLeftTag = FALSE;
+  pNode->bRightTag = FALSE;
+  
+  status = createTreeRecursive(&pNode->pLeftChild, pData, pCurIndex);
+  if (status != OK)
+    return status;
+  status = createTreeRecursive(&pNode->pRightChild, pData, pCurIndex);
+  if (status != OK)
+    return status;
+
+  *ppNode = pNode;
+  
+  return OK;
+}
+
+void preOrderTraverse(LP_T_BI_TREE_NODE pTree, VISIT_FUNC visit)
+{
+  visit(pTree->pData);
+  if (!pTree->bLeftTag && pTree->pLeftChild)
+    preOrderTraverse(pTree->pLeftChild, visit);
+  if (!pTree->bRightTag && pTree->pRightChild)
+    preOrderTraverse(pTree->pRightChild, visit);
+}
+
+void inOrderTraverse(LP_T_BI_TREE_NODE pTree, VISIT_FUNC visit)
+{
+  if (!pTree->bLeftTag && pTree->pLeftChild)
+    inOrderTraverse(pTree->pLeftChild, visit);
+  visit(pTree->pData);
+  if (!pTree->bRightTag && pTree->pRightChild)
+    inOrderTraverse(pTree->pRightChild, visit);
+}
+
+
+void destroyTree(LP_T_BI_TREE_NODE pRoot)
+{
+  if (!pRoot->bLeftTag && pRoot->pLeftChild)
+    destroyTree(pRoot->pLeftChild);
+  if (!pRoot->bRightTag && pRoot->pRightChild)
+    destroyTree(pRoot->pRightChild);
+  free(pRoot->pData);
+  free(pRoot);
+}
+static LP_T_BI_TREE_NODE pre;
+void thread(LP_T_BI_TREE_NODE pRoot)
+{
+  if (pRoot->pLeftChild)
+    thread(pRoot->pLeftChild);
+  if (pre->pRightChild == NULL)
+    {
+      pre->pRightChild = pRoot;
+      pre->bRightTag = TRUE;
+    }
+  if (pRoot->pLeftChild == NULL)
+    {
+      pRoot->pLeftChild = pre;
+      pRoot->bLeftTag = TRUE;
+    }
+  pre = pRoot;
+  if (pRoot->pRightChild)
+    thread(pRoot->pRightChild);
+}
+
+void threadingTree(LP_T_BI_TREE_NODE pRoot, LP_T_BI_TREE_NODE *ppThreadRoot)
+{
+
+  pre = (LP_T_BI_TREE_NODE)malloc(sizeof(T_BI_TREE_NODE));
+  if (NULL == pre)
+    {
+      printf("Out Of Memory in Line %d, Function %s", __LINE__, __FUNCTION__);
+      exit(OVERFLOW);
+    }
+
+  *ppThreadRoot = pre;
+
+  pre->pLeftChild = pRoot;
+  pre->bLeftTag = FALSE;
+  thread(pRoot);
+  assert(pre->pRightChild == NULL);
+  pre->pRightChild = *ppThreadRoot;
+  pre->bRightTag = TRUE;
+  (*ppThreadRoot)->pRightChild = pre;
+}
+
+
+void threadVisit(LP_T_BI_TREE_NODE pThreadRoot, VISIT_FUNC visit)
+{
+  LP_T_BI_TREE_NODE pIterNode;
+  pIterNode = pThreadRoot->pLeftChild;
+  while (pIterNode != pThreadRoot)
+    {
+      while (!pIterNode->bLeftTag && pIterNode->pLeftChild)
+	pIterNode = pIterNode->pLeftChild;
+      visit(pIterNode->pData);
+      while (pIterNode->bRightTag && pIterNode->pRightChild != pThreadRoot)
+	{
+	  pIterNode = pIterNode->pRightChild;
+	  visitNode(pIterNode->pData);
+	}
+      pIterNode = pIterNode->pRightChild;
+    }
+}
