3203355cca8b27c07afe224b03bbe9afe8046366
croseadu <croseadu@93ef7e32-9b48-0410-81a5-0f72e0cf0767>
2011-04-08 02:55:45 +0000

    Finished Sparse Matrix reverse

--- trunk/google/basicAlgorithm/GList.c
+++ trunk/google/basicAlgorithm/GList.c
@@ -1,1 +1,28 @@
+#include "../include/common.h"
 
+
+typedef struct _GLIST 
+{
+  BOOL bIsAtom;
+  union
+  {
+    char value;
+    struct
+    {
+      struct _GLIST *hp;
+      struct _GLIST *tp;
+    }list;
+  }data;
+}GLIST, *LP_GLIST;
+
+
+int main()
+{
+
+
+
+
+
+
+  return 0;
+}
