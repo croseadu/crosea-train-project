0aea9b7ca3068d2a3ec2de9a39204a1cd2144522
croseadu@gmail.com <croseadu@gmail.com@93ef7e32-9b48-0410-81a5-0f72e0cf0767>
2013-08-08 22:11:36 +0000

    work on glist, finished the first version.

---
+++ trunk/googleAgain/basic/GList.c
@@ -0,0 +1,93 @@
+#include "../include/mytype.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char ElementType;
+
+typedef struct _GNode
+{
+  bool isAtom;
+  union {
+    ElementType value;
+    struct _GNode *pNode;
+  };
+  struct _GNode *pNext;
+}GNode, *LPGNode;
+
+
+void dumpGList(LPGNode pList)
+{
+  LPGNode pIter = NULL;
+  if (pList == NULL)
+    return;
+
+  pIter = pList;
+  while (pIter != NULL) {
+    if (pIter->isAtom)
+      putchar(pIter->value);
+    else {
+      putchar('(');
+      dumpGList(pIter->pNode);
+      putchar(')');
+    }
+    pIter = pIter->pNext;
+    if (pIter)
+      putchar(',');
+  }
+}
+
+void skipSpace(const char **ppStr)
+{
+  const char *p = *ppStr;
+  while (*p !='\0' && *p == ' ')
+    ++p;
+  *ppStr = p;
+}
+
+LPGNode createGList(const char **ppStr)
+{
+  LPGNode pNode;
+  LPGNode *ppInsert;
+  skipSpace(ppStr);
+  
+  if (**ppStr == '(') {
+    ++*ppStr;
+    skipSpace(ppStr);
+    pNode = malloc(sizeof(GNode));
+    pNode->isAtom = false;
+    pNode->pNode = NULL;
+    pNode->pNext = NULL;
+
+    ppInsert = &pNode->pNode;
+    while(**ppStr != ')') {
+      *ppInsert = createGList(ppStr);
+      skipSpace(ppStr);
+      if (**ppStr == ',')
+	++*ppStr;
+      ppInsert = &(*ppInsert)->pNext;
+    }
+    *ppInsert = NULL;
+    ++*ppStr;
+  }
+  else{
+    pNode = (LPGNode)malloc(sizeof(GNode));
+    pNode->isAtom = true;
+    pNode->value = **ppStr;
+    pNode->pNext = NULL;
+    ++*ppStr;
+  }
+
+  return pNode;
+}
+
+int main()
+{
+  LPGNode pNode;
+  //const char *p = "(a,(b),(e,(c,())))";
+  const char *p = "((a,(),(e,f)),(b,(c,d,(g,h,(k,i))),e),())";
+  pNode = createGList(&p);
+  putchar('\n');
+  dumpGList(pNode);
+  putchar('\n');
+  return 0;
+}
